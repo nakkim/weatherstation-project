95d9770cb9923e3cf0839fe797d6206137c950b7
Ville Ilkka <ville.iv.ilkka@gail.com>
2020-03-29 15:51:38 +0300

    Add missing translations

--- frontend/src/translations.js
+++ frontend/src/translations.js
@@ -4,12 +4,14 @@ var translations = {
     'latest': 'Latest observations',
     'measured': 'measured',
     'loading': 'Loading observations...',
+    'unable': 'Unable to load observations',
     'temperature': 'Temperature',
     'light': 'Amount of light',
     'humidity': 'Relative humidity',
     'pressure': 'Air pressure',
     'noData': 'No data to display',
-    'useAvg': 'Use running average in graph'
+    'useAvg': 'Use running average in graph',
+    'timeSeries': 'Load the entire time series'
   },
 
   'fi': {
@@ -17,12 +19,14 @@ var translations = {
     'latest': 'Viimeisimmät havainnot',
     'measured': 'mitattu',
     'loading': 'Ladataan havaintoja...',
+    'unable': 'Havaintoja ei saatu ladattua',
     'temperature': 'Lämpötila',
     'light': 'Valon määrä',
     'humidity': 'Suhteellinen kosteus',
     'pressure': 'Ilmanpaine',
     'noData': 'Havaintoja ei löytynyt',
-    'useAvg': 'Käytä juoksevaa keskiarvoa kuvaajassa'
+    'useAvg': 'Käytä juoksevaa keskiarvoa kuvaajassa',
+    'timeSeries': 'Lataa koko aikasarja'
   }
 }
 
