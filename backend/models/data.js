44a6aab79fb3d056640c8baca290af629f3bc7d9
Ville Ilkka <ville.iv.ilkka@gail.com>
2020-03-29 15:53:31 +0300

    Change data module to use mongodb instance hosted on mondodb atlas and add data expire option to data schema

--- backend/models/data.js
+++ backend/models/data.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose')
 
-const url = 'mongodb://handler:arduino1@ds157707.mlab.com:57707/weatherobs'
+// const url = 'mongodb://handler:arduino1@ds157707.mlab.com:57707/weatherobs'
+const url = 'mongodb+srv://handler:arduino@cluster0-tmh36.mongodb.net/test?retryWrites=true&w=majority'
 
-mongoose.connect(url, { useNewUrlParser: true })
+mongoose.connect(url, { useNewUrlParser: true, useCreateIndex: true })
 
 const schema = new mongoose.Schema({
   time: Number,
@@ -10,7 +11,8 @@ const schema = new mongoose.Schema({
   humidity: Number,
   pressure: Number,
   light: Number,
-  timeString: String
+  timeString: String,
+  createdAt: { type: Date, expires: 8640000}
 }, {timestamps: true});
 
 const Value = mongoose.model('Value', schema)
